72826f3ca622ac7f3fbb242792d05dea2c0bb32b
Xaqui <xoaquin.bg@gmail.com>
2023-09-25 10:12:13 +0200

    Clase 25-09-23

--- 001/main.js
+++ 001/main.js
@@ -1,8 +1,17 @@
+var x = 0;
+
 function setup() {
     createCanvas(400, 400);
   }
   
   function draw() {
+    if(x <= 400){
+      x +=10;
+    } else {
+      x = 0;
+    }
     background(220,100,200);
-    circle(200,200,20);
+    circle(x,200,20);
+    console.log(x);
+    frameRate(2);
   }
